906d18dea95a73abf73579ae6bde349628469fc3
yi <117130908+unkown-jp@users.noreply.github.com>
2024-03-02 19:28:26 +0900

    FIXME Step-5-2-1, FIXME Step-5-2-2,

--- src/skillcheck/service/BaseService.java
+++ src/skillcheck/service/BaseService.java
@@ -56,14 +56,14 @@ public abstract class BaseService {
         try {
             // FIXME Step-5-2-1: postgresqlのドライバー名を記述しなさい。
         	// Tips: ConstSQLより適切な定数を参照
-            Class.forName([ここへ記述]);
+            Class.forName(ConstSQL.JDBC_POSTGRES_DRIVER);
 
             // FIXME Step-5-2-2: DBへ接続するための初期設定を記述しなさい。
             // Tips: ConstSQLより適切な定数を参照
             this.connection = DriverManager.getConnection(
-            		[ここへ記述],
-            		[ここへ記述],
-            		[ここへ記述]);
+            		ConstSQL.JDBC_CONNECTION,
+            		ConstSQL.JDBC_POSTGRES_USER,
+            		ConstSQL.JDBC_POSTGRES_PASS);
 
             // オートコミットOFF
             this.connection.setAutoCommit(false);
